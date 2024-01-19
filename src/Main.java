ee518a8ce44fd476eaae437da60713c95c08b283
PNRRUS <pomaikai7378@gmail.com>
2024-01-19 22:45:48 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+String name;
+name = "gitHub";
+            System.out.println("Hello world!"); //команда вывода
 
-        System.out.println("Hello world!");
     }
-}
+
+
+    }
