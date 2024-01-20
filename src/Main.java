f32a0122f2903b803cd49af167f9845d9778eb10
PNRRUS <pomaikai7378@gmail.com>
2024-01-20 21:35:57 +0300

    Commit_6_For_1<100Line_Romas_Project

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,11 @@ name = "gitHub";
             System.out.println("Hello world!"); //команда вывода
 System.out.println(name);
 for (int i = 1;i <= 100;i++){
-    System.out.println(i);
+    System.out.println();
+    for(int j = 1;j <= 8;j++){
+        System.out.print(i  + " ");
+    }
+
 }
     }
 
