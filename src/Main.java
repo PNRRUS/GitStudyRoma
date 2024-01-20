f7adee61c8ae3b3a5d7e63985dce9a396f4e15e5
PNRRUS <pomaikai7378@gmail.com>
2024-01-20 21:25:56 +0300

    Commit_5_For_1<100_Romas_Project

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ String name;
 name = "gitHub";
             System.out.println("Hello world!"); //команда вывода
 System.out.println(name);
-for (int i = 1;i <= 5;i++){
+for (int i = 1;i <= 100;i++){
     System.out.println(i);
 }
     }
